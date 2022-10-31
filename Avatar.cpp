af79d1951d482f1ddcfde7b54b6b5a6535700a9c
LuciaGonzalezMoreno <ml_gonzalezmoreno@hotmail.com>
2022-10-30 22:48:05 -0300

    Actualizacion del reloj y nombre
    Falta probar el reloj

--- Avatar.cpp
+++ Avatar.cpp
@@ -1,4 +1,5 @@
 #include "Avatar.h"
+#include <string>
 
 Avatar::Avatar()
 {
@@ -12,27 +13,30 @@ Avatar::Avatar()
     _textoNombre.setFont(_fuente1);
     _textoCronometro.setFont(_fuente1);
 
-
+    _nombreJugador ="Jugador";
+    _textoNombre.setCharacterSize(15);
+    _textoNombre.setColor(sf::Color::Black);
     _textoNombre.setString(_nombreJugador);
-    _textoNombre.setOrigin(0 , _textoNombre.getGlobalBounds().height);
-    _textoNombre.setPosition(_spriteAvatar.getPosition().x + 5.f , _spriteAvatar.getPosition().y + 5.f);
+    _textoNombre.setOrigin(_textoNombre.getGlobalBounds().width, _textoNombre.getGlobalBounds().height);
+    _textoNombre.setPosition(_spriteAvatar.getPosition().x + 110.f, _spriteAvatar.getPosition().y -28.f);
 
-
-   _textoCronometro.setString("18:00");
-   _textoCronometro.setOrigin(0 , _textoCronometro.getGlobalBounds().height);
-   _textoCronometro.setPosition(_spriteAvatar.getPosition().x , _spriteAvatar.getPosition().y);
+    _textoCronometro.setString("18:00");
+    _textoCronometro.setCharacterSize(15);
+    _textoCronometro.setColor(sf::Color::Black);
+    _textoCronometro.setOrigin(0, _textoCronometro.getGlobalBounds().height);
+    _textoCronometro.setPosition(_spriteAvatar.getPosition().x + 53.f, _spriteAvatar.getPosition().y -14.f);
 }
 
 void Avatar::update()
 {
-    //_textoCronometro.setString(_tiempo2.));
+    _textoCronometro.setString(std::to_string(_tiempo2.asSeconds()));
 }
 
 void Avatar::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-   target.draw(_spriteAvatar, states);
-    target.draw(_textoNombre , states);
-    target.draw(_textoCronometro , states);
+    target.draw(_spriteAvatar, states);
+    target.draw(_textoNombre, states);
+    target.draw(_textoCronometro, states);
 }
 
 void Avatar::setNombreJugador(std::string nombre)
